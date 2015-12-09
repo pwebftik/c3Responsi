a3e6c7493933e94f77674a81ce57448231bbc483
rizkyfadhilah <rizkyfadhilah90@gmail.com>
2015-12-09 19:06:34 +0700

    tugas modul 8

---
+++ Rezky_Fadhilah_105/modul8/update.php
@@ -0,0 +1,32 @@
+<?php
+
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db_name = "db_praktikum";
+
+if(!$koneksi = mysqli_connect($host,$user,$pass,$db_name)){
+	die("koneksi putus karena : ".mysqli_connect_error());
+}
+// perintah menambahkan data 
+if(isset($_GET['id'])){
+	if( $_GET['id']=== 'update'){
+		//perintah dasar update data
+		$id = $_GET['id'];
+		$sql = "UPDATE user SET username = 'jaka',nama='sembung' where id='1'";
+		
+		if(mysqli_query($koneksi,$sql)){
+			//pesan yang akan muncul ketika query berhasil
+			echo "yes,data berhasil di input broooooo!!!!";
+				}else{
+					//pesan yang akan muncul ketika query gagal di eksekusi
+					echo "oh shit gagal !".mysqli_error($koneksi);
+				}	
+	
+	}else {
+		echo "salah alamat bro coba cek sini :";
+}
+}
+
+?>
